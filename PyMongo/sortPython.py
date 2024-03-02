5d7df2b013f0f1abd623ad88b55d5a876e8f4213
chandrikaguntupalli <71213679+chandrikaguntupalli@users.noreply.github.com>
2024-03-01 19:51:14 -0500

    sortPython.py

---
+++ PyMongo/sortPython.py
@@ -0,0 +1,32 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import datetime
+
+uri = "mongodb+srv://chandrikaguntupalli2:chandu99@cluster0.trp076i.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    # Send a ping to confirm a successful connection
+    client.admin.command('ping')
+
+    # Get reference to 'sample_training' database
+    db = client.sample_training
+
+    # Get reference to 'zips' collection
+    accounts_collection = db.zips
+
+    new = [{"$sort":{"pop":-1}}]
+
+    result = list(accounts_collection.aggregate(new))
+
+    if result:
+        print(result)
+    else:
+        print("No documents in the collection")
+
+
+except Exception as e:
+    print(e)
+finally:
+    client.close()
